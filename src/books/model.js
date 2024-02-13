7bd6d136a7deb65a70891bb77f29d11f2c56a21d
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-02-13 13:48:12 +0000

    chore: add db connection

---
+++ src/books/model.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../db/connection");
+
+const Book = sequelize.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    defaultValue: "some author",
+  },
+  genre: {
+    type: DataTypes.STRING,
+    defaultValue: "some genre",
+  },
+});
+
+module.exports = Book;
